deee72ba60adb09a2a45731d6d520f4264fc0e88
Tahreem-riaz <totehreem101@gmail.com>
2026-09-23 22:37:34 +0500

    Add the set of student ID's

--- 03_sets.py
+++ 03_sets.py
@@ -65,3 +65,13 @@ print("Student Skills:", skills)
 #
 #     Create a set containing repeated IDs and
 #     print the final set.
+
+student_ids = {
+    101,
+    102,
+    103,
+    101,
+    104,
+    102,
+    105
+}
